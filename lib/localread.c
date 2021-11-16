26e48c5b3b080bb346896f1e7f7dc0b1710b55e5
Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
2021-11-16 18:17:21 -0300

    Fixes localread.c

--- lib/localread.c
+++ lib/localread.c
@@ -1,1 +1,59 @@
 #include "libinput.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 1000
+
+void carrega_dados (char *caminho_dos_dados, int *linhas, int *colunas, void **planilha, char **nomes_linhas[]) {
+	float planilha_template[MAX][MAX];
+	char nomes_linhas_template[MAX][100];
+	char *linha, *coluna;
+	int i=0, j;
+	size_t len;
+	FILE *arqptr = fopen(caminho_dos_dados, "r");
+	while (getline(&linha, &len, arqptr) != -1) {
+		j = 0;
+		coluna = strtok(linha, ",");
+		while(coluna != NULL) {
+			if(j==0) {
+				strcpy(nomes_linhas_template[i], coluna);
+			} else {
+				if(coluna == "") continue;
+				planilha_template[i][j-1] = atof(coluna);
+			}
+			coluna = strtok(NULL, ",");
+			j++;
+		}
+		i++;
+	}
+	*linhas = i;
+	*colunas = j-1;
+	float p[*linhas][*colunas];
+	for(int i=0; i<*linhas; i++){
+		for(int j=0; j<*colunas; j++){
+			p[i][j] = planilha_template[i][j];
+		}
+	}
+	*planilha = (void *)(float **)p;
+	*nomes_linhas = nomes_linhas_template;
+	fclose(arqptr);
+}
+
+int main() {
+	void *planilha;
+	int r, c;
+	char *nome_linhas[100];
+	char *url = "BRICS_PIBPerCapita.csv";
+	carrega_dados(url, &r, &c, &planilha, &nome_linhas);
+	float matriz[r][c];
+	for (int i=0; i<r;i++){
+		printf("%s ", *nome_linhas+i*100);
+		for(int j=0; j<c;j++){
+			matriz[i][j] = *((float*) planilha+(i*c)+j);
+			printf("%f ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
