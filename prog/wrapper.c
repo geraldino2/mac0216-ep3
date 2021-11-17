78fd8ac2ee2ee54b6dbb00e522c18a83d1475d04
larissavmss <larissavitoriamedeiros@usp.br>
2021-11-17 15:41:38 -0300

    Update main with "carrega_dados"

--- prog/wrapper.c
+++ prog/wrapper.c
@@ -1,3 +1,39 @@
+#include <stdio.h>
+//#include "libinput.h"
+//#include "libplot.h"
+
 int main() {
+	printf("Informe o caminho para o arquivo csv: ");
+	char path[100];
+	scanf("%[^\n]", path);
+
+	int colunas, linhas;
+    void *planilha;
+	char *nomelinhas[100];
+	
+	carrega_dados(path,&linhas ,&colunas, &planilha, &nomelinhas);
+
+	float matriz[linhas][colunas];
+    for (int j=0; j<linhas;j++){
+        for(int i=0; i<colunas;i++){
+            matriz[j][i]= *((float*)planilha+(j*colunas)+i);
+        }
+    }
+	char rotuloy[100], rotulox[100];
+	printf("Qual o rotulo das abcissas?");
+	scanf("%[^\n]", rotulox);
+	printf("E das ordenadas?");
+	scanf("%[^\n]", rotuloy);
+
+	char **nome_linhas;
+	nome_linhas = malloc(linhas * sizeof *nome_linhas);
+	
+	for (int i=0; i<linhas; i++){
+		nome_linhas[i] = *nomelinhas+i*100;
+	}
+
+	//Plota gráfico
+
+
 	return 0;
 }
