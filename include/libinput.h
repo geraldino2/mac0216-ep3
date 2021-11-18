566deeb296f72e2df147c58bfb27c3551ecfa740
larissavmss <larissavitoriamedeiros@usp.br>
2021-11-18 10:50:38 -0300

    Update interfaces

--- include/libinput.h
+++ include/libinput.h
@@ -1,1 +1,16 @@
+#ifndef _libinput_h
+#define _libinput_h
+
+/*
+ * Função: carrega_dados
+ * Uso: carrega_dados(caminho_dos_dados, &linhas, &colunas, &planilha, &nome_linhas)
+ * ----------------------------------
+ * Esta função devolve o tamanho da planilha correspondente nos parâmetros de saída
+ * *linhas e *colunas, um apontador para a posição de memória da planilha e outro apontador
+ * para um vetor de strings contendo os nomes das linhas em nomes_linhas.
+ *  
+*/
+
 void carrega_dados (char *caminho_dos_dados, int *linhas, int *colunas, void **planilha, char **nomes_linhas[]);
+
+#endif
--- include/libplot.h
+++ include/libplot.h
@@ -1,5 +1,44 @@
+#ifndef _libinput_h
+#define _libinput_h
+
+/**
+ * Função: define_titulo
+ * Uso: 
+ * ------------------------
+ * 
+ */
 void define_titulo (char *titulo);
+
+/**
+ * Função: define_rotulo_x
+ * Uso: 
+ * ------------------------
+ * 
+ */
 void define_rotulo_x (char *nome);
+
+/**
+ * Função: define_rotulo_y
+ * Uso: 
+ * ------------------------
+ * 
+ */
 void define_rotulo_y (char *nome);
+
+/**
+ * Função: define_nomes_linhas
+ * Uso: 
+ * ------------------------
+ * 
+ */
 void define_nomes_linhas (char *nomes_linhas[]);
+
+/**
+ * Função: desenha_grafico
+ * Uso: 
+ * ------------------------
+ * 
+ */
 void desenha_grafico (int linhas, int colunas, float planilha[][colunas]);
+
+#endif
